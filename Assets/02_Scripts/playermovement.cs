82f41a1bdcd97baaae434865ed2855a0fb34b7d2
Marius Häusler <mhaeusler04@gmail.com>
2026-03-12 16:16:00 +0100

    Added EnemySpawner to Main

--- Assets/02_Scripts/playermovement.cs
+++ Assets/02_Scripts/playermovement.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody _rb;
-    private Vector3 _left = Vector3.forward * 2;
-    private Vector3 _right = Vector3.back * 2;
+    private Vector3 _left = Vector3.forward * 5;
+    private Vector3 _right = Vector3.back * 5;
     
     void Start()
     {
