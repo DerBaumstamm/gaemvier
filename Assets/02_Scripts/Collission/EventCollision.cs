f8f9ad19fe19fc650f77b35198c4ff2999378933
VincentKurth <147550031+VincentKurth@users.noreply.github.com>
2026-03-12 13:14:55 +0100

    PlayerCollision
    Macht zwei Events on Player Collision für "Enemy" und "Gate"

---
+++ Assets/02_Scripts/Collission/EventCollision.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+public class EventCollision : MonoBehaviour
+{
+    public event Action OnEnemyCollision; 
+    public event Action OnGateCollision;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))  
+        {
+            OnEnemyCollision?.Invoke();
+            Debug.Log("Spieler hat Enemy getroffen!");
+        }
+        else if (collision.gameObject.CompareTag("Gate"))
+        {
+            OnGateCollision?.Invoke();
+            Debug.Log("Spieler hat Gate getroffen!");
+        }
+        
+    }
+}
