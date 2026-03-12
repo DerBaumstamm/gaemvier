8b9debf25fe2300f60284d40febf5180905b6e38
Marius Häusler <mhaeusler04@gmail.com>
2026-03-12 16:51:54 +0100

    added Enemy Despawn Condition

--- Assets/02_Scripts/EnemyMovement.cs
+++ Assets/02_Scripts/EnemyMovement.cs
@@ -6,9 +6,17 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody rb;
 
     // Update is called once per frame
-void Awake()
+    void Awake()
     {
         rb = GetComponent<Rigidbody>();
         rb.AddForce(Vector3.left * speed, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.z < -6 || transform.position.z > 6 || transform.position.x < -60f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
